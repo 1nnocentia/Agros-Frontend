1da5845e97f543b11f185f3a3375ce6b51aae6df
inno <innocentiahan14@gmail.com>
2025-12-12 22:18:38 +0800

    feat: integrate Firebase services with authentication and AI capabilities

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:agros/core/theme/app_theme.dart';
 // import 'package:agros/presentation/views/porcupine_page.dart';
 // import 'package:agros/presentation/viewmodels/porcupine_viewmodel.dart';
@@ -13,6 +16,9 @@ import 'package:agros/presentation/viewmodels/stt_viewmodel.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(
     MultiProvider(
       providers: [
---
+++ lib/presentation/viewmodels/chat_viewmodel.dart
@@ -0,0 +1,44 @@
+import 'package:agros/core/services/ai_service.dart';
+import 'package:agros/data/models/chat_message.dart';
+import 'package:flutter/material.dart';
+
+class ChatViewmodel extends ChangeNotifier {
+  final AiService _aiService = AiService();
+
+  final List<ChatMessage> _messages = [];
+  bool _isAiTyping = false;
+
+  List<ChatMessage> get messages => _messages;
+  bool get isAiTyping => _isAiTyping;
+
+  Future<void> sendUserMessage(String text) async {
+    if (text.trim().isEmpty) return;
+
+    _addMessage(text, true);
+    
+    _isAiTyping = true;
+    notifyListeners();
+
+    try {
+      final responseText = await _aiService.sendMessage(text);
+      
+      _addMessage(responseText, false);
+    } catch (e) {
+      _addMessage("Maaf, terjadi kesalahan: $e", false, isError: true);
+    } finally {
+      _isAiTyping = false;
+      notifyListeners();
+    }
+  }
+
+  void _addMessage(String text, bool isUser, {bool isError = false}) {
+    _messages.add(ChatMessage(
+      id: DateTime.now().millisecondsSinceEpoch.toString(),
+      text: text,
+      isUser: isUser,
+      timestamp: DateTime.now(),
+      isError: isError,
+    ));
+    notifyListeners();
+  }
+}
